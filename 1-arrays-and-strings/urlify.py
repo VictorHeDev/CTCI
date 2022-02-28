31e0fbff73c00f37483a3dd44a6911d9a51e65a9
Woodstock <victorhe97@gmail.com>
2022-02-28 18:41:16 -0500

    Complete urlify

--- 1-arrays-and-strings/urlify.py
+++ 1-arrays-and-strings/urlify.py
@@ -5,6 +5,37 @@ additional characters, and that you are given the "true" length of the string.
 '''
 
 
-def urlify(string):
+'''
+Solution 1: 2-scan approach
+1. Count the number of spaces. Triple the number and compute how many extra
+chars we will have in the final string
+2. Iterate in reverse order, and edit the string. When we see a space,
+replace it with %20, else replace with original char
+'''
+
+
+def urlify_algo(string, length):
+    # convert string to list bc strings are immutable in Python
+    char_list = list(string)
+    new_idx = len(char_list)
+
+    for i in reversed(range(length)):
+        if char_list[i] == ' ':
+            # replace spaces
+            char_list[new_idx - 3: new_idx] = '%20'
+            new_idx -= 3
+        else:
+            # move chars
+            char_list[new_idx - 1] = char_list[i]
+            new_idx -= 1
+
+    return "".join(char_list[new_idx:])
+
+
+def urlify_pythonic_alt(string, length):
+    return string[:length].replace(" ", '%20')
+
+
+def urlify_pythonic(string):
     strArr = string.split(" ")
     return '%20'.join(strArr)
