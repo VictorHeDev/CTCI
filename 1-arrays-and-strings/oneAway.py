e39bc1c86bf67847f483110af6ee009651f58c70
Woodstock <victorhe97@gmail.com>
2022-03-01 16:26:46 -0500

    Complete oneAway

--- 1-arrays-and-strings/oneAway.py
+++ 1-arrays-and-strings/oneAway.py
@@ -5,15 +5,47 @@ write a function to check if they are one edit (or zero edits) away
 '''
 
 
-def oneAway(str1, str2):
-    if str1 == str2:
-        return True
-    set1 = set(str1)
-    set2 = set(str2)
+def one_edit_away(str1, str2):
+    if len(str1) == len(str2):
+        return one_edit_replace(str1, str2)
+    elif len(str1) + 1 == len(str2):
+        return one_edit_insert(str1, str2)
+    elif len(str1) - 1 == len(str2):
+        return one_edit_insert(str2, str1)
+    else:
+        return False
 
-    return abs(len(set1) - len(set2)) <= 1
+
+def one_edit_replace(s1, s2):
+    '''
+    Replacement happens when the strings are different in only one place
+    '''
+    found_difference = False
+    for i in range(len(s1)):
+        if s1[i] != s2[i]:
+            if found_difference:
+                return False
+        found_difference = True
+    return True
 
 
-print(oneAway('pale', 'ple'))
-print(oneAway('pale', 'pale'))
-print(oneAway('pale', 'bake'))
+def one_edit_insert(s1, s2):
+    '''
+    Insertions and Removal are essentially the same (inverse of each other)
+    s2 is the longer string compared to s1
+    '''
+    idx1, idx2 = 0, 0
+    while idx2 < len(s2) and idx1 < len(s1):
+        if s1[idx1] != s2[idx2]:
+            if idx1 != idx2:
+                return False
+            idx2 += 1
+        else:
+            idx1 += 1
+            idx2 += 1
+    return True
+
+
+print(one_edit_away('pale', 'ple'))
+print(one_edit_away('pale', 'pale'))
+print(one_edit_away('pale', 'bake'))
