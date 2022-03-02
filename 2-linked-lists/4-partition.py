a8b549a5f3dd10a97d645f79dc672535f7147d4c
Woodstock <victorhe97@gmail.com>
2022-03-02 12:57:04 -0500

    Complete partition

--- 2-linked-lists/4-partition.py
+++ 2-linked-lists/4-partition.py
@@ -9,5 +9,32 @@ right partitions.
 EXAMPLE
 INPUT:  3 -> 5 -> 8 -> 5 -> 10 -> 2 -> 1 [partition = 5]
 OUTPUT: 3 -> 1 -> 2 -> 10 -> 5 -> 5 -> 8
+
+APPROACH:
+- instead of shifting and swapping elements, we can actually create 2
+different LL and combine them at the end
+- one LL will include elements less than the partition
+- the other LL will include elements greater or equal to than the partition
 '''
 
+
+def partition(ll, target):
+    current = ll.head = ll.tail
+
+    while current:
+        next_node = current.next
+        current.next = None
+
+        if current.value < target:
+            # insert node at head
+            current.next = ll.head
+            ll.head = current
+        else:
+            # insert node at tail
+            ll.tail.next = current
+            ll.tail = current
+        current = next_node
+
+    # error checking in case all nodes are less than target
+    if ll.tail.next is not None:
+        ll.tail.next = None
