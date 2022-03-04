d8d56b97cb1584c82fb90a77cde83a6b22d681ee
Woodstock <victorhe97@gmail.com>
2022-03-03 20:51:39 -0500

    Solution for loop detection

--- 2-linked-lists/8-loop_detection.py
+++ 2-linked-lists/8-loop_detection.py
@@ -14,3 +14,24 @@ APPROACH
   - they will meet at the start of loop
 '''
 
+
+def loop_detection(ll):
+    fast = slow = ll.head
+
+    while fast and fast.next:
+        fast = fast.next.next
+        slow = slow.next
+
+        if fast is slow:
+            break
+
+    # if there is no cycle
+    if fast is None or fast.next is None:
+        return None
+
+    slow = ll.head
+    while fast is not slow:
+        fast = fast.next
+        slow = slow.next
+
+    return fast
