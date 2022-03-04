d6231b07806f9669e685fc3ca03ab7b84af96a57
Woodstock <victorhe97@gmail.com>
2022-03-04 00:55:30 -0500

    Complete pseudocode for stack min

--- 3-stacks-and-queues/1-three_in_one.py
+++ 3-stacks-and-queues/1-three_in_one.py
@@ -6,6 +6,7 @@ Describe how you would use a single array to implement three stacks
 '''
 SOLUTION 1: Fixed Division
 - divide the array into 3 equal parts and allow the individual stack to grow in that limited space.
+- for example, set num_stacks = 3, stack_size = 6
 '''
 
 
--- 3-stacks-and-queues/2-stack_min.py
+++ 3-stacks-and-queues/2-stack_min.py
@@ -1,3 +1,16 @@
 '''
 How would you design a stack which, in addition to push and pop, has a function min which returns the minimum element? Push, pop, and min should all operate in O(1) time.
 '''
+
+
+'''
+APPROACH
+- minimums only change when elements are added
+- after an element is popped, the new minimum will be the last minimum before the current element was popped
+- to ensure constant lookup time for minimum, what we can do is have the "nodes" keep track of the local minimum
+
+- in a separate solution, what if we instead had 2 stacks?
+- one stack to keep track of the original elements added
+- second stack to keep track of new minimums that will be popped off when elements from og stack are popped off (if they match)
+'''
+
