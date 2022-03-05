5e365c2f10ca90b6aebb21c4dbd1fba858f1e992
Woodstock <victorhe97@gmail.com>
2022-03-05 11:17:08 -0500

    Approach for check balanced trees

--- 4-trees_and_graphs/12-paths_with_sum.py
+++ 4-trees_and_graphs/12-paths_with_sum.py
@@ -7,3 +7,4 @@ from parent nodes to child nodes).
 '''
 
 
+
--- 4-trees_and_graphs/4-check_balanced.py
+++ 4-trees_and_graphs/4-check_balanced.py
@@ -1,3 +1,13 @@
 '''
-Implement a function to check if a binary tree is balanced. For the purposes of this question, a balanced tree is defined to be a tree such that the hights of the two subtrees of any node never differ by more than one.
+Implement a function to check if a binary tree is balanced. For the purposes
+of this question, a balanced tree is defined to be a tree such that the
+heights of the two subtrees of any node never differ by more than one.
+
+APPROACH:
+- recurse through the entire tree and for each node compute the heights of
+each subtree
+- if the heights of subtrees are different by > 1 then return early
 '''
+
+
+
