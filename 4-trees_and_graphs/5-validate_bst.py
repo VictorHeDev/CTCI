98dbe317e77d55cbbe598e174b3f6eac4cf1699f
Woodstock <victorhe97@gmail.com>
2022-03-06 11:47:41 -0400

    Complete successor

--- 4-trees_and_graphs/5-validate_bst.py
+++ 4-trees_and_graphs/5-validate_bst.py
@@ -11,6 +11,9 @@ APPROACH 2:
 - leverage definition of a bst
 - must satisfy the condition left.data <= current.data < right.data for
 each node
+- more precise definition--all left nodes must be <= current node, current node must be < all right nodes
+- we cn pass down the min and max values, and as we iterate through the tee we can verify against progressively narrower ranges
+- when we branch left, the max gets updated, when we branch right, the min gets updated. If anything fails then we stop and return false
 '''
 
 
@@ -23,3 +26,19 @@ def validate_tree_node(node, left_bound, right_bound):
         return True
 
     return node.data >= left_bound and node.data <= right_bound and validate_tree_node(node.left, left_bound, node.data) and validate_tree_node(node.right, node.data, right_bound)
+
+
+# solution
+def check_bst(root):
+    return _check_best(root, null, null)
+
+
+def _check_bst(node, min, max):
+    if node is None:
+        return True
+
+    if (min is not None and node.data <= min) or (max is not None and node.data > max):
+        return False
+    if not _check_best(node.left, min, node.data) or not _check_bst(node.right, node.data, max):
+        return False
+    return True
--- 4-trees_and_graphs/6-successor.py
+++ 4-trees_and_graphs/6-successor.py
@@ -1,3 +1,36 @@
 '''
-Write an algorithm to find the "next" node (i.e., in-order successor) of a given node in a binary search tree. You may assume that each node has a link to its parent.
+Write an algorithm to find the "next" node (i.e., in-order successor) of
+a given node in a binary search tree. You may assume that each node has
+a link to its parent.
 '''
+
+
+def successor(node):
+    if not node:
+        return None
+
+    child = node.right
+    if child:
+        while child.left:
+            child = child.left
+    if child:
+        return child
+    parent = node.parent
+    child = node
+    if parent and parent.left == child:
+        return parent
+    if parent and parent.right == child:
+        while parent and parent.left != child:
+            child = parent
+            parent = child.parent
+        return parent
+
+
+class Node():
+    def __init__(self, data, left=None, right=None) -> None:
+        self.data, self.left, self,right = data, left, right
+        self.parent = None
+        if self.left:
+            self.left.parent = self
+        if self.right:
+            self.right.parent = self
