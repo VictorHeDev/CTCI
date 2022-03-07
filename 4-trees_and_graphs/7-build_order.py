6f727d74a9a42e79b315559d6441373605a4ea8b
Woodstock <victorhe97@gmail.com>
2022-03-07 12:06:28 -0400

    Add approach for build order

--- 4-trees_and_graphs/7-build_order.py
+++ 4-trees_and_graphs/7-build_order.py
@@ -6,4 +6,13 @@ INPUT:
   projects: a, b, c, d, e, f
   dependencies: (a, d), (f, b), (b, d), (f, a), (d, c)
 OUTPUT: f, e, a, b, d, c
+
+APPROACH 1 - topological sort algorithm
+APPROACH 2
+- DFS to find the build path
+- when we get to the end of the path and can't go any further, we know those terminating nodes can be the last projects to be built
+- think about cycles -- don't want to get stuck in an infinite route
+
 '''
+
+
